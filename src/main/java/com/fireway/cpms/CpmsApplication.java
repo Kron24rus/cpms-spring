34b7cf5ae21cdd1df7888859939ff016e9df6671
Alexander Mikheev <alexandr.mikheev@soft-werke.com>
2018-06-12 21:47:56 +0300

    Working with mysql DB.

--- src/main/java/com/fireway/cpms/CpmsApplication.java
+++ src/main/java/com/fireway/cpms/CpmsApplication.java
@@ -1,23 +1,55 @@
 package com.fireway.cpms;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @RestController
-@EnableAutoConfiguration
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class})
 public class CpmsApplication {
+
+	@Autowired
+	private Environment env;
 
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
 	}
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(CpmsApplication.class, args);
+	}
+
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		// See: application.properties
+		dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(env.getProperty("spring.datasource.url"));
+		dataSource.setUsername(env.getProperty("spring.datasource.username"));
+		dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+		System.out.println("## getDataSource: " + dataSource);
+
+		return dataSource;
 	}
 }
